2076776e7aa2a63d4e28366a9fba373adbaf75aa
felipx <felipemonterobruni@gmail.com>
2023-04-14 14:12:58 -0300

    Se agrega driver de aplicacion en c.

---
+++ currencies.py
@@ -0,0 +1,25 @@
+import json
+import requests
+  
+# Defining Binance API URL
+key = "https://api.binance.com/api/v3/ticker/price?symbol="
+  
+# Making list for multiple crypto's
+currencies = ["BTCUSDT", "DOGEUSDT", "LTCUSDT"]
+j = 0
+  
+# running loop to print all crypto prices
+#for i in currencies:
+#    
+#    # completing API for request
+#    url = key+currencies[j]  
+#    data = requests.get(url)
+#    data = data.json()
+#    print(data)
+#    j = j+1
+#    #print(f"{data['symbol']} price is {data['price']}")
+
+url = key+currencies[0]
+data = requests.get(url)
+data = data.json()
+print(data)
---
+++ src/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void usage(char* argv0);
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+        usage(argv[0]);
+    
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        perror("pipe error");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t chdpid = fork();
+    if (chdpid < 0) {
+        perror("fork error");
+        exit(EXIT_FAILURE);
+    }
+
+    if (chdpid == 0) {
+
+        if (dup2(pipefd[1], STDOUT_FILENO) < 0) {
+            perror("dup2 error");
+            exit(EXIT_FAILURE);
+        }
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        const char* args[3] = {"/usr/bin/python", argv[1], (char*)NULL};
+
+        if (execv(args[0], (char**)args) < 0) {
+            char buffer[13] = "execv error\n";
+            write(STDOUT_FILENO, buffer, sizeof(buffer));
+            perror("execv error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+
+        close(pipefd[1]);
+
+        char buffer[1024];
+        memset(buffer, 0x00, sizeof(buffer));
+        int i = 0;
+        while (read(pipefd[0], buffer + i, 1) > 0) {
+            i++;
+        }
+        
+        close(pipefd[0]);
+
+        printf("%s", buffer);
+
+    }
+}
+
+
+void usage(char* argv0)
+{
+    fprintf(stderr, "Usage: %s <string>\n", argv0);
+    exit(EXIT_FAILURE);
+}
